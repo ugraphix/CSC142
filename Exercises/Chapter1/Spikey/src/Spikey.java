5a019cc7ca9bd161adf94ea26135481c843895b3
ugraphix <grace@ugraphix.com>
2018-01-02 19:50:26 -0800

    Initial commit

---
+++ Exercises/Chapter1/Spikey/src/Spikey.java
@@ -0,0 +1,15 @@
+public class Spikey {
+
+	public static void main(String[] args) {
+		// Exercise 2
+		
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
+		System.out.println("\\\\\\///");
+		System.out.println("///\\\\\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
+
+	}
+
+}
---
+++ Exercises/Chapter1/Stewie/src/Stewie.java
@@ -0,0 +1,12 @@
+public class Stewie {
+
+	public static void main(String[] args) {
+		// Exercise 1
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("//////////////////////");
+
+
+
+	}
+}
