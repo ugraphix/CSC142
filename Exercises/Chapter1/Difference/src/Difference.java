8e7f92365d1ed791b1fdacefe2c5a7b4fbb770a3
ugraphix <grace@ugraphix.com>
2018-01-02 20:39:17 -0800

    exercise uploads
    exercises 3 & 4

---
+++ Exercises/Chapter1/Difference/src/Difference.java
@@ -0,0 +1,15 @@
+
+public class Difference {
+
+	public static void main(String[] args) {
+		// Exercise 4
+		
+		System.out.println("What is the difference between "
+				+ "\na ' and a \"? Or between a \" and a \\\" ? ");
+		System.out.println("");
+		System.out.println("One is what we see when we're typing our program. "
+				+ "\nThe other is what appears on the \"console.\"");
+
+	}
+
+}
---
+++ Exercises/Chapter1/WellFormed/src/WellFormed.java
@@ -0,0 +1,19 @@
+
+public class WellFormed {
+
+	public static void main(String[] args) {
+		// Exercise 3
+		
+				System.out.println("A well-formed Java program has "
+						+ "\na main method with { and }"
+						+ "\nbraces.");
+				System.out.println("");
+				System.out.println("A System.out.println statement "
+						+ "\nhas ( and ) and usually a "
+						+ "\nString that starts and ends"
+						+ "\nwith a \" character."
+						+ "\n(But we type \\\" instead!)");
+						
+	}
+
+}
